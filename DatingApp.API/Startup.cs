1742827d0db0bc83e4f84f52e034f16e256ad2cf
Attila Poroszkai <poroszkai.attila@gmail.com>
2019-10-23 16:45:44 +0200

    Finishing off the Member edit component

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -88,7 +88,10 @@ namespace DatingApp.API
             }
 
             // app.UseHttpsRedirection();
-            app.UseCors(x => x.AllowAnyOrigin().AllowAnyHeader());
+            app.UseCors(
+                x => x.AllowAnyOrigin()
+                      .AllowAnyHeader()
+                      .AllowAnyMethod());
             app.UseAuthentication();
             app.UseMvc();
         }
